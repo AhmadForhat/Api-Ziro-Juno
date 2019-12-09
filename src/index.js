1f4d9371572227de20933f72724b2f88551d6eb7
AhmadForhat <ahmad.forhat@gmail.com>
2019-12-09 11:04:31 -0300

    Utilização do querystring para facilitar as querys e organização do código

--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
 const express = require('express');
 const app = express();
-const fs = require('fs')
+const querystring = require('querystring');
 const request = require('request');
 
-// Teste pra ficar mais "organizado"
+// Testando querystring
+
 let basicUrl = 'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/list-charges';
-const token = '?token=FF66FF607414B9F657DB2439BCF4D1E3F964BB323D62EF756B038352CA650189';
-const beginDueDate = '&beginDueDate=03/12/2019'
-const endDueDate = '&endDueDate=05/12/2019';
-const beginPaymentDate = '&beginPaymentDate=03/12/2019';
-const endPaymentDate = '&endPaymentDate=05/12/2019';
-const beginPaymentConfirmation = '&beginPaymentConfirmation=03/12/2019';
-const endPaymentConfirmation = '&endPaymentConfirmation=05/12/2019';
-const query = token+beginDueDate+endDueDate+beginPaymentDate+endPaymentDate+beginPaymentConfirmation+endPaymentConfirmation;
-let url = basicUrl + query;
+const query = querystring.stringify(
+    {   token: 'FF66FF607414B9F657DB2439BCF4D1E3F964BB323D62EF756B038352CA650189',
+        beginDueDate: '07/12/2019',
+        endDueDate: '08/12/2019',
+        beginPaymentDate: '07/12/2019',
+        endPaymentDate: '08/12/2019',
+        beginPaymentConfirmation: '07/12/2019',
+        endPaymentConfirmation: '08/12/2019'
+     });
+const url = basicUrl + '?' + query;
 
 // Salvar a requisição em uma pasta local.
 
 // request(url).pipe(fs.createWriteStream('boletos2.json')) // https://github.com/request/request
 
-
-const apiJuno = request({
-    url: url,
-    json: true
-},(err, res, body) =>{
-    if(err){
-        console.log(err);
-        return err
-    }else{
-        console.log(JSON.stringify({ body }, undefined, 1));
-        app.get('/', (req,res) => {
-            res.json([ body.data.charges ])
-        })
-        app.post('/post', (req,res) => {
-            res.json([ body.data.charges ])
-        })
-    }
-});
+app.get('/consulta-pagamentos', (req,res) => {
+    request({
+        url:url,
+        json: true
+    },(e,r,body) => {
+        if(e){
+            if(e.code == 'ENOTFOUND')
+                console.log(e);
+                res.json("Nome do erro: " + e.code+ " " + "Descrição: " + "Url não encontrada");
+            if(e.code != 'ENOTFOUND'){
+                console.log(e);
+                res.json(e.code)
+            }
+        }else{
+        console.log(JSON.stringify({ body }))
+        res.json(body);
+    }})
+})
 
 app.listen(3000, () => {
     console.log('Sucess');
