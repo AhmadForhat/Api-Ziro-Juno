f7f3a6e914608dd20e5bf3790c47cdaae8472ce4
AhmadForhat <ahmad.forhat@gmail.com>
2019-12-10 11:09:30 -0300

    Refatorando para promises

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const request = require('request');
 
 // Testando querystring
 
-let basicUrl = 'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/list-charges';
+    let basicUrl = 'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/list-charges';
 const query = querystring.stringify(
     {   token: 'FF66FF607414B9F657DB2439BCF4D1E3F964BB323D62EF756B038352CA650189',
         beginDueDate: '07/12/2019',
@@ -21,35 +21,55 @@ const url = basicUrl + '?' + query;
 
 // request(url).pipe(fs.createWriteStream('boletos2.json')) // https://github.com/request/request
 
-app.get('/consulta-pagamentos', (req,res) => {
-    request({
-        url:url,
-        json: true
-    },(e,r,body) => {
-        if(e){
-            if(e.code == 'ENOTFOUND')
-                console.log(e);
-                res.json({"Nome do erro": e.code,
-                            "Descrição": "Url não encontrada"});
-            if(e.code != 'ENOTFOUND'){
-                console.log(e);
-                res.json(e.code)
+function requisicao(){
+    return new Promise(function resolvePromise(resolve,reject){
+        request({
+            url:url,
+            json: true
+        },(e,r,body) => {
+            if(e){
+                    if(e.code == 'ENOTFOUND'){
+                        console.log(e);
+                        res.json({"Nome do erro": e.code,
+                        "Descrição": "Url não encontrada"})};
+                    if(e.code != 'ENOTFOUND'){
+                        console.log(e);
+                     res.json(e.code)
+                }
+            }else{
+                const ids = [];
+                for(let i in body.data.charges){
+                const id = body.data.charges[i];
+                ids.push(id.code)
             }
-        }else{
-            const ids = [];
-            for(let i in body.data.charges){
-            const id = body.data.charges[i];
-            ids.push(id.code)
+                const date = [];
+                for(let i in body.data.charges){
+                const id = body.data.charges[i];
+                date.push(id.dueDate)
             }
-            const date = [];
-            for(let i in body.data.charges){
-            const id = body.data.charges[i];
-            date.push(id.dueDate)
+                return resolve([ ids, date ]);
             }
-            console.log(JSON.stringify({ body }))
-            res.json([ ids, date ]);
-    }})
-})
+        })
+    })
+}
+
+function getApi(body){
+        return app.get('/consulta-pagamentos', (req,res) => {
+            res.json(body)
+        })
+}
+
+main()
+async function main(){
+    try{
+        const resultado = await requisicao()
+        const puxaApi = await getApi(resultado)
+        console.log(resultado)
+    }
+    catch(error){
+        console.log('Deu error' , error)
+    }
+}
 
 app.listen(3000, () => {
     console.log('Sucess');
