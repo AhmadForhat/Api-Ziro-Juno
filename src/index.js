52474fdf1aba80679641ad2a630eb4a7a1742589
AhmadForhat <ahmad.forhat@gmail.com>
2019-12-11 11:20:56 -0300

    Adicação de tratamentos de erro

--- src/index.js
+++ src/index.js
@@ -6,39 +6,16 @@ const request = require('request-promise-native');
 // Testando querystring
 
 app.get('/consulta-pagamentos', async (req,res) => {
-    const result = await request("https://viacep.com.br/ws/01001000/json/");
-    console.log(result);
     const basicUrl = 'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/list-charges';
     const query = querystring.stringify(req.query);
     const url = `${basicUrl}?${query}`;
-    console.log(url);
     // console.log(Object.keys(req));
-    // console.log('headers', req.headers);
-    // console.log('url', req.url);
-    // console.log('baseUrl', req.baseUrl);
-    // console.log('query', req.query);
-    // // console.log('res', req.res);
-    // console.log('params', req.params);
-    // console.log('originalUrl', req.originalUrl);
-    // console.log('route', req.route);
-    request({
+    console.log(req.route.path)
+    const resultado = await request({
         url:url,
         json: true // testar sem
     })
     .then((body) => {
-        // console.log(e)
-        // console.log(r)
-        // console.log(body)
-        // if(e){
-        //     if(e.code == 'ENOTFOUND')
-        //         console.log(e);
-        //         res.json({"Nome do erro": e.code,
-        //                     "Descrição": "Url não encontrada"});
-        //     if(e.code != 'ENOTFOUND'){
-        //         console.log(e);
-        //         res.json(e.code)
-        //     }
-        // }else{
             const ids = [];
             for(let i in body.data.charges){
             const id = body.data.charges[i];
@@ -53,8 +30,15 @@ app.get('/consulta-pagamentos', async (req,res) => {
             res.json([ ids, date ]);
     })
     .catch((error) => {
-        console.log(error)
-        // res.json(`Código do erro: ${error.cause.code}`)
+        if(error.statusCode == 400){
+            res.json([error.error.errorMessage])
+        // }else if(error.error == ""){
+        //     res.json(`Erro na solicitação, rever o que quer buscar`)
+        }
+        else{
+            // res.json([`${error.cause.code} URL base com problema, favor solicitar alteração no código`])
+            res.json({error})
+        }
     })
 })
 
