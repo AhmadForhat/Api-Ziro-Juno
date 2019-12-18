53600a89a452c0eb6c8b1142445702dc7bcb3b28
AhmadForhat <ahmad.forhat@gmail.com>
2019-12-18 09:38:45 -0300

    tratativa de erro da consulta pagamentos e deixando a mesma com formato async

--- routes/consultaPag.js
+++ routes/consultaPag.js
@@ -8,40 +8,34 @@ router.get('/consulta-pagamentos', async (req,res) => {
     const basicUrl = 'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/list-charges';
     const query = querystring.stringify(req.query);
     const url = `${basicUrl}?token=${process.env.TOKEN}&${query}`;
-    console.log(url)
-    // console.log(Object.keys(req));
-    console.log(req.route.path)
+
     let options = {
         url:url,
         json: true
-    }
+    };
+    try {
 
-    rp(options)
-    .then((body) => {
-            const ids = [];
-            for(let i in body.data.charges){
-            const id = body.data.charges[i];
-            ids.push(id.code)
-            }
-            const date = [];
-            for(let i in body.data.charges){
-            const id = body.data.charges[i];
-            date.push(id.dueDate)
-            }
-            console.log(JSON.stringify({ body }))
-            res.json([ ids, date ]);
-    })
-    .catch((error) => {
+        let data = await rp(options);
+        const ids = [];
+        for(let i in data.data.charges){
+        const id = data.data.charges[i];
+        ids.push(id.code)
+        }
+        const date = [];
+        for(let i in data.data.charges){
+        const id = data.data.charges[i];
+        date.push(id.dueDate)
+        }
+        res.json([ ids, date ]);
+
+    } catch (err) {
         if(error.statusCode == 400){
             res.json([error.error.errorMessage])
-        // }else if(error.error == ""){
-        //     res.json(`Erro na solicitação, rever o que quer buscar`)
         }
         else{
-            // res.json([`${error.cause.code} URL base com problema, favor solicitar alteração no código`])
             res.json({error})
         }
-    })
+    }
 })
 
 module.exports = router
