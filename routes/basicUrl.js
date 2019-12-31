acc98cbd07deb128a77e5a99814c67310cfa5ba7
unknown <ahmad.forhat@gmail.com>
2019-12-31 08:21:12 -0300

    Subindo servirdor com url diferente

--- routes/basicUrl.js
+++ routes/basicUrl.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const tokenJuno = process.env.TOKEN
 
-const basicUrl = (caminho,query) => `https://sandbox.boletobancario.com/boletofacil/integration/api/v1/${caminho}?token=${tokenJuno}&${query}`
+const basicUrl = (caminho,query) => `https://www.boletobancario.com/boletofacil/integration/api/v1/${caminho}?token=${tokenJuno}&${query}`
 
 module.exports = basicUrl
