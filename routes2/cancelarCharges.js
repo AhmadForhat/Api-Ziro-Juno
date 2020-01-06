5d5934f489555e120743bddacd209c33d981e618
unknown <ahmad.forhat@gmail.com>
2020-01-06 10:12:15 -0300

    Geração de token automatica

--- routes2/cancelarCharges.js
+++ routes2/cancelarCharges.js
@@ -1,11 +1,26 @@
 const querystring = require('querystring')
 const rp = require('request-promise-native')
-require('dotenv').config()
-
-const tokenJuno = process.env.TOKEN
-const acessToken = process.env.acessToken
 
 const cancelCharges =  async (req,res) => {
+    // Gerando o token
+    require('dotenv').config()
+    const UrlToken = "http://localhost:3000/geracao-token?grant_type=client_credentials"
+    const tokenJuno = process.env.TOKEN
+    const usernameToken = process.env.userToken
+    const passwordToken = process.env.pdwToken
+    const authToken = "Basic " + new Buffer.from(usernameToken + ":" + passwordToken).toString("base64");
+    const optionsToken = {
+        method: 'GET',
+        url:UrlToken,
+        headers :{
+            "Authorization": authToken
+        },
+        json: true
+    }
+    let dataToken = await rp(optionsToken)
+    const acessToken = `Bearer ${dataToken.data.access_token}`
+    console.log(acessToken)
+    // Requisição de cancelamento
     const basicUrl = "https://sandbox.boletobancario.com/api-integration/charges"
     const query = (req.query.id)
     const url = `${basicUrl}/${query}/cancelation`
--- routes2/consultaCharges.js
+++ routes2/consultaCharges.js
@@ -1,11 +1,25 @@
 const querystring = require('querystring')
 const rp = require('request-promise-native')
-require('dotenv').config()
-
-const tokenJuno = process.env.TOKEN
-const acessToken = process.env.acessToken
 
 const consultaCharges =  async (req,res) => {
+    // Gerando o token
+    require('dotenv').config()
+    const UrlToken = "http://localhost:3000/geracao-token?grant_type=client_credentials"
+    const usernameToken = process.env.userToken
+    const passwordToken = process.env.pdwToken
+    const authToken = "Basic " + new Buffer.from(usernameToken + ":" + passwordToken).toString("base64");
+    const optionsToken = {
+        method: 'GET',
+        url:UrlToken,
+        headers :{
+            "Authorization": authToken
+        },
+        json: true
+    }
+    let dataToken = await rp(optionsToken)
+    const acessToken = `Bearer ${dataToken.data.access_token}`
+    // Requisição de consulta
+    const tokenJuno = process.env.TOKEN
     const basicUrl = "https://sandbox.boletobancario.com/api-integration/charges"
     const query = querystring.stringify(req.query)
     const url = `${basicUrl}?${query}`
@@ -22,6 +36,7 @@ const consultaCharges =  async (req,res) => {
 
     try {
         let data = await rp(options)
+        console.log("Requisição feita com sucesso!")
         res.json({data})
     } catch (err) {
         res.json({err})
