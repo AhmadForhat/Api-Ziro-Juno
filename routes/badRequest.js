fb0c88b0cd7c0cc32c433b752bb6a879276be9f9
unknown <ahmad.forhat@gmail.com>
2020-01-10 09:44:15 -0300

    Tirando os console.log

--- routes/badRequest.js
+++ routes/badRequest.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
-const badRequest = async (req, res) => {
-	console.log(res.status)
+const badRequest = async (req, res) => {s
 	res.status(404).send('Rota inválida. Favor consultar a documentação https://documenter.getpostman.com/view/9467330/SWECVaMU?version=latest')
 }
 
